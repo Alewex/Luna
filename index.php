7b4717819f13d182932976d36e0dde3787137103
Alewex <alex.eepsilon@gmail.com>
2015-06-10 12:55:43 -0500

    Cleaner
    Index file is now cleaner

--- index.php
+++ index.php
@@ -2,33 +2,9 @@
 
 $luna = require 'bootstrap.php';
 
-$app = $luna['Core'];
-$response = $luna['Response'];
-
 $app->get(['/', '/home'], function()
 {
 	echo "Welcome!";
-});
-
-$app->get('/calendar', function()
-{
-	echo "Calendar page.";
-});
-
-$app->get('/contact', function()
-{
-	echo "Contact page.";
-});
-
-$app->get('/about', function()
-{
-	echo "About page.";
-});
-
-$app->get('/404', function() use($response)
-{
-	$response->code(404);
-	echo "<b>DEMO</b> Page not found.";
 });
 
 $app->dispatch();
