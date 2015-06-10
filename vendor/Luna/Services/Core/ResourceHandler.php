46e869ea27595652348dde46d6a83a383b18a313
Alewex <alex.eepsilon@gmail.com>
2015-06-10 13:21:03 -0500

    Tada!

--- vendor/Luna/Services/Core/ResourceHandler.php
+++ vendor/Luna/Services/Core/ResourceHandler.php
@@ -5,19 +5,19 @@ namespace Luna\Services\Core;
 class ResourceHandler
 {
 
-	public $path = 'vendor/Luna/Resources/';
+	private static $path = 'vendor/Luna/Resources/';
 	public $resource;
 
 	public function path($path)
 	{
-		$this->path = $path;
+		return self::$path = $path;
 	}
 
 	public function load($resource)
 	{
 		$this->resource = $resource;
 
-		$path = glob($this->path . $resource . '.*');
+		$path = glob(self::$path . $resource . '.*');
 
 		if ($path) require_once $path[0];
 	}
