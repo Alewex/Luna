e07ed3db32add40b8a3087ed028ca5032907b68b
Alewex <alex.eepsilon@gmail.com>
2015-06-10 12:55:14 -0500

    Added service variables
    Added the service variables in the bootstrap file to make the index file
    cleaner.

--- bootstrap.php
+++ bootstrap.php
@@ -4,7 +4,12 @@ require 'config/autoloader.php';
 
 $services = [
 	'Core' => new Luna\Services\Core\Luna(),
-	'Response' => new Luna\Services\HTTP\Response()
+	'Response' => new Luna\Services\HTTP\Response(),
+	'Resource' => new Luna\Services\Core\ResourceHandler()
 ];
+
+$app = $services['Core'];
+$response = $services['Response'];
+$resource = $services['Resource'];
 
 return $services;
