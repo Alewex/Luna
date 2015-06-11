fab2d6bbde3b2bafdf15be0e01c8436440b065cd
Alewex <alex.eepsilon@gmail.com>
2015-06-11 18:33:13 -0500

    Created the Controller class.

---
+++ vendor/Luna/Services/HTTP/Controllers/Controller.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Luna\Services\HTTP\Controllers;
+
+use Luna\Services\Core\ResourceHandler;
+
+class Controller
+{
+
+	public $class;
+	public $method;
+
+	public function __construct($class, $method)
+	{
+		$this->class = $class;
+		$this->method = $method;
+		$this->resource = new ResourceHandler;
+
+		$this->loadController();
+	}
+
+	public function loadController()
+	{
+		$path = $this->resource->getResourcePath('controllers/' . $this->class . '.php');
+
+		if ($this->controllerExists($path[0]))
+		{
+			require_once $path[0];
+
+			$controller = new $this->class;
+
+			if ($this->methodOfControllerExists($controller))
+			{
+				return $controller->{$this->method}();
+			} else
+			{
+				return $controller->main();
+			}
+		}
+	}
+
+	public function controllerExists($pathToController)
+	{
+		return (file_exists($pathToController) ? true : false);
+	}
+
+	public function methodOfControllerExists($controller)
+	{
+		return (method_exists($controller, $this->method) ? true : false);
+	}
+
+}
