85b0560d4216307bf5a816a39cb0d7e43042ec40
Alewex <alex.eepsilon@gmail.com>
2015-06-11 18:31:33 -0500

    Added support for the ViewHandler and ControllerHandler

--- bootstrap.php
+++ bootstrap.php
@@ -5,11 +5,15 @@ require 'config/autoloader.php';
 $services = [
 	'Core' => new Luna\Services\Core\Luna(),
 	'Response' => new Luna\Services\HTTP\Response(),
-	'Resource' => new Luna\Services\Core\ResourceHandler()
+	'View' => new Luna\Services\HTTP\Views\ViewHandler(),
+	'Resource' => new Luna\Services\Core\ResourceHandler(),
+	'Controller' => new Luna\Services\HTTP\Controllers\ControllerHandler()
 ];
 
 $app = $services['Core'];
+$view = $services['View'];
 $response = $services['Response'];
 $resource = $services['Resource'];
+$controller = $services['Controller'];
 
 return $services;
