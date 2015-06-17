cf2f8499c624c7429cd60895ab72d36a05bb1578
Alewex <alex.eepsilon@gmail.com>
2015-06-17 17:54:54 -0500

    Added Models Support

--- vendor/Luna/Services/HTTP/Controllers/BaseController.php
+++ vendor/Luna/Services/HTTP/Controllers/BaseController.php
@@ -2,6 +2,7 @@
 
 namespace Luna\Services\HTTP\Controllers;
 
+use Luna\Services\Database\ModelHandler as Model;
 use Luna\Services\HTTP\Views\ViewHandler as View;
 
 class BaseController
@@ -10,6 +11,7 @@ class BaseController
 	public function __construct()
 	{
 		$this->view = new View();
+		$this->model = new Model();
 	}
 
 }
