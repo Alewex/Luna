68479cd14661b63b410b6e3f4b08c50445206ebf
Alewex <alex.eepsilon@gmail.com>
2015-06-17 17:51:49 -0500

    Added an example route.
    This route loads a controller and the controller loads a model to
    display users in a database.

--- index.php
+++ index.php
@@ -9,4 +9,9 @@ $app->get(['/', '/home'], function() use ($controller)
 	$controller->load('WelcomeController');
 });
 
+$app->get('/users', function() use($controller)
+{
+	$controller->load('UsersController');
+});
+
 $app->dispatch();
