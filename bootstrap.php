651f87b35ac6e8b10d164a0e9cc4a97607432422
Alewex <alex.eepsilon@gmail.com>
2015-06-18 16:01:58 -0500

    Added the ModelHandler

--- bootstrap.php
+++ bootstrap.php
@@ -8,12 +8,14 @@ $service = new Luna\Core\ServiceContainer();
 $service->register('Core', new Luna\Core\Luna());
 $service->register('Response', new Luna\HTTP\Response());
 $service->register('View', new Luna\HTTP\Views\ViewHandler());
+$service->register('Model', new Luna\Database\ModelHandler());
 $service->register('Resource', new Luna\Core\ResourceHandler());
 $service->register('Controller', new Luna\HTTP\Controllers\ControllerHandler());
 $service->register('Fluent', new FluentPDO\FluentPDO(new PDO("mysql:host=$db[host];dbname=$db[db]", "$db[user]", "$db[password]")));
 
 $app = $service->load('Core');
 $view = $service->load('View');
+$model = $service->load('Model');
 $fluent = $service->load('Fluent');
 $response = $service->load('Response');
 $resource = $service->load('Resource');
