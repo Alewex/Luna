78bdf0cac6754704248122c63ca7194bc68af36a
Alewex <alex.eepsilon@gmail.com>
2015-06-18 21:43:44 -0500

    Cleaned up the index file.

--- index.php
+++ index.php
@@ -1,19 +1,12 @@
 <?php
 
-$luna = require 'bootstrap.php';
+// Bootstrap
+require_once 'bootstrap.php';
 
+/**
+ *	Uncomment the line below if you want to set a different
+ *	resource path.
+ */
 // $resource->path('path/to/resources/');
-
-$app->get(['/', '/home'], function() use($app, $view)
-{
-	$view->render('Welcome', 'Luna', ['framework' => 'Luna', 'version' => 1.8, 'routes' => $app->router->collection->routes]);
-});
-
-$app->get('/users', ['controller' => 'UsersController']);
-
-$app->get('/repo', function() use($response)
-{
-	$response->redirect('https://github.com/Alewex/Luna');
-});
 
 $app->dispatch();
