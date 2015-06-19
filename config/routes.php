d4aec64e9679e783c7f79ffb008f1bf57392f248
Alewex <alex.eepsilon@gmail.com>
2015-06-18 21:43:15 -0500

    Created a separate routes file.
    Now all the routes are stored in this file.

---
+++ config/routes.php
@@ -0,0 +1,13 @@
+<?php
+
+$app->get(['/', '/home'], function() use($app, $view)
+{
+	$view->render('Welcome', 'Luna', ['framework' => 'Luna', 'version' => 1.8, 'routes' => $app->router->collection->routes]);
+});
+
+$app->get('/users', ['controller' => 'UsersController']);
+
+$app->get('/repo', function() use($response)
+{
+	$response->redirect('https://github.com/Alewex/Luna');
+});
