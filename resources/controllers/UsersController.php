40178575a9232f00708cb48428426b0e41735120
Alewex <alex.eepsilon@gmail.com>
2015-06-18 23:04:27 -0500

    User creation function added in sample UsersController.

--- resources/controllers/UsersController.php
+++ resources/controllers/UsersController.php
@@ -13,4 +13,23 @@ class UsersController extends BaseController
 		$this->view->render('Users', 'Users', ['users' => $allUsers]);
 	}
 
+	public function Create()
+	{
+		$this->view->render('CreateUser', 'Create User');
+	}
+
+	public function CreateUser()
+	{
+		$name = $_POST['name'];
+		$age = $_POST['age'];
+		$location = $_POST['location'];
+
+		$users = $this->model->load('UsersModel')->model;
+
+		if ($users->createUser($name, $age, $location))
+		{
+			Luna\Core\ServiceContainer::load('Response')->redirect($_SERVER['ROOT'] . '/users');
+		}
+	}
+
 }
