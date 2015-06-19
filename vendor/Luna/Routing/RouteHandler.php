e17003774ea459486286455171558c4492eae03f
Alewex <alex.eepsilon@gmail.com>
2015-06-18 22:07:57 -0500

    Added route and response processing to the RouteHandler.

--- vendor/Luna/Routing/RouteHandler.php
+++ vendor/Luna/Routing/RouteHandler.php
@@ -18,4 +18,46 @@ class RouteHandler
 		return $this->collection->add($route);
 	}
 
+	public function processRoute($method, $route, $response)
+	{
+		if (!is_callable($response) && is_array($response))
+		{
+			$response = $this->processResponse($response);
+		}
+
+		if (is_array($route))
+		{
+			foreach ($route as $route)
+			{
+				$this->addToCollection(new Route($method, $route, $response));
+			}
+		} else
+		{
+			$this->addToCollection(new Route($method, $route, $response));
+		}
+	}
+
+	public function processResponse(array $response)
+	{
+		$type = array_keys($response)[0];
+		$action = array_values($response)[0];
+
+		switch ($type)
+		{
+			case 'controller':
+				return function() use ($action) { ServiceContainer::$services['Controller']->load($action); };
+			break;
+
+			case 'view':
+				if (is_array($action))
+				{
+					return function() use ($action) { ServiceContainer::$services['View']->render($action['view'], (isset($action['title']) ? $action['title'] : null), (isset($action['data']) ? $action['data'] : null)); };
+				} else
+				{
+					return function() use ($action) { ServiceContainer::$services['View']->render($action); };
+				}
+			break;
+		}
+	}
+
 }
