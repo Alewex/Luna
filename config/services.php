f5f4f2038feb0cdd799be1e87a3213ba7e111ea7
Alewex <alex.eepsilon@gmail.com>
2015-06-18 22:05:39 -0500

    Created a separate services file.

--- config/services.php
+++ config/services.php
@@ -1,21 +1,21 @@
 <?php
 
+use Luna\Core\ServiceContainer as Service;
+
 $db = require_once 'config/.database.php';
 
-$service = new Luna\Core\ServiceContainer();
+Service::register('Core', new Luna\Core\Luna());
+Service::register('View', new Luna\Views\ViewHandler());
+Service::register('Response', new Luna\HTTP\Response());
+Service::register('Model', new Luna\Database\ModelHandler());
+Service::register('Resource', new Luna\Core\ResourceHandler());
+Service::register('Controller', new Luna\HTTP\Controllers\ControllerHandler());
+Service::register('Fluent', new FluentPDO\FluentPDO(new PDO("mysql:host=$db[host];dbname=$db[db]", "$db[user]", "$db[password]")));
 
-$service->register('Core', new Luna\Core\Luna());
-$service->register('View', new Luna\Views\ViewHandler());
-$service->register('Response', new Luna\HTTP\Response());
-$service->register('Model', new Luna\Database\ModelHandler());
-$service->register('Resource', new Luna\Core\ResourceHandler());
-$service->register('Controller', new Luna\HTTP\Controllers\ControllerHandler());
-$service->register('Fluent', new FluentPDO\FluentPDO(new PDO("mysql:host=$db[host];dbname=$db[db]", "$db[user]", "$db[password]")));
-
-$app = $service->load('Core');
-$view = $service->load('View');
-$model = $service->load('Model');
-$fluent = $service->load('Fluent');
-$response = $service->load('Response');
-$resource = $service->load('Resource');
-$controller = $service->load('Controller');
+$app = Service::load('Core');
+$view = Service::load('View');
+$model = Service::load('Model');
+$fluent = Service::load('Fluent');
+$response = Service::load('Response');
+$resource = Service::load('Resource');
+$controller = Service::load('Controller');
