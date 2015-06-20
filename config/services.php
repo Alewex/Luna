129e343656ffa09bb5343b8d9c9cefdea2f047b9
Alex Epps <alex.eepsilon@gmail.com>
2015-06-19 20:28:57 -0500

    Updated namespacing.

--- config/services.php
+++ config/services.php
@@ -10,7 +10,7 @@ Service::register('Response', new Luna\HTTP\Response());
 Service::register('Model', new Luna\Database\ModelHandler());
 Service::register('Resource', new Luna\Core\ResourceHandler());
 Service::register('Controller', new Luna\HTTP\Controllers\ControllerHandler());
-Service::register('Fluent', new FluentPDO\FluentPDO(new PDO("mysql:host=$db[host];dbname=$db[db]", "$db[user]", "$db[password]")));
+Service::register('Fluent', new FluentPDO(new PDO("mysql:host=$db[host];dbname=$db[db]", "$db[user]", "$db[password]")));
 
 $app = Service::load('Core');
 $view = Service::load('View');
